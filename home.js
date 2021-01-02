f91b213b011ace5b7f0fbb85cc7f72d5b2d24961
Rajiv Aryal <58936200+Aryal-rajiv@users.noreply.github.com>
2021-01-02 19:26:55 +0545

    Beginner
    Just the beginner project

---
+++ home.js
@@ -0,0 +1,7 @@
+console.log('lets start');
+//alert('Rajiv the hacker');
+var G = 'Gundruk';
+console.log(G);
+var domain = prompt('What is your domain');
+document.getElementById('someText').innerHTML = domain;
+console.log(domain);
